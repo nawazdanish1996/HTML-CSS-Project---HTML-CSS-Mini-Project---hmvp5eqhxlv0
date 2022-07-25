2dd9a263c617a09679130645e7383d8dc4a500f8
Nawaz Danish <nawazdanish1996@gmail.com>
2022-07-26 00:54:54 +0530

    add

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Change Background Color and icon
 var icon = document.getElementById('icona');
 var boda = document.querySelector('body');
+var nav2 = document.getElementById('nav2');
 icon.onclick = function(){
 
     document.body.classList.toggle('dark-theme');
@@ -8,6 +9,7 @@ icon.onclick = function(){
     if(document.body.classList.contains('dark-theme')){
         icon.src = "https://raw.githubusercontent.com/nawazdanish1996/HTML-CSS-Project---HTML-CSS-Mini-Project---hmvp5eqhxlv0/master/dark%20theme%20icon/moon.png";
         boda.style.background = "#111111";
+        nav2.style.background = "#111111"
         
     }else{
         icon.src = "https://raw.githubusercontent.com/nawazdanish1996/HTML-CSS-Project---HTML-CSS-Mini-Project---hmvp5eqhxlv0/master/dark%20theme%20icon/sun.png";
