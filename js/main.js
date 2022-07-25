4cd3097217c5c08e6e72cf884742dd8258337d6b
Nawaz Danish <nawazdanish1996@gmail.com>
2022-07-25 17:05:49 +0530

    add

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ icon.onclick = function(){
     document.body.classList.toggle('dark-theme');
 
     if(document.body.classList.contains('dark-theme')){
-        icon.src = "/dark theme icon/moon.png";
+        icon.src = "https://raw.githubusercontent.com/nawazdanish1996/HTML-CSS-Project---HTML-CSS-Mini-Project---hmvp5eqhxlv0/master/dark%20theme%20icon/moon.png";
         boda.style.background = "#111111";
         
     }else{
-        icon.src = "/dark theme icon/sun.png";
+        icon.src = "https://raw.githubusercontent.com/nawazdanish1996/HTML-CSS-Project---HTML-CSS-Mini-Project---hmvp5eqhxlv0/master/dark%20theme%20icon/sun.png";
         boda.style.background = "#192a56";
     }
 }
